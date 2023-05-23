ebe2f1d5bbdc90a26c9b43e8689263ebbd3e4ed6
Florentino Tuason <tinoysoft@gmail.com>
2023-05-23 12:56:11 +0800

    added code for warm up #3

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿//get input from user
+Console.Write("Enter x: ");
+int input = Convert.ToInt32(Console.ReadLine());
+
+void write(int i)
+{
+    for (int x = 1; x <= i; x++)
+    {
+        Console.Write("{0} ", x * i);
+    }
+    Console.WriteLine("");
+}
+
+int counter = 1;
+int interval = 1;
+
+while (counter > 0)
+{
+    write(counter);
+
+    //loop through number range
+    counter = counter + interval;
+    if (counter > input)
+    {
+        counter = input - 1;
+        interval = interval * -1;
+    }
+}
